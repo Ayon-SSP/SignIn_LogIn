d1b84e1ba4581aa00c9953c810300d0fefa44e58
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2022-08-17 14:15:46 +0530

    projectdone

--- Content/Configuration.py
+++
@@ -1,5 +0,0 @@
-hostname = 'localhost'
-database = 'For_Practice'
-username = 'postgres'
-pwd = '321654'
-port_id = 5432
---
+++ Content/DatabaseFill.py
@@ -0,0 +1,7 @@
+
+# Fill It and uncomment the details
+hostname = 'localhost'
+# database = {YourDatabaseName}
+username = 'postgres'
+# pwd = {passowrd}
+# port_id = {port_id}
