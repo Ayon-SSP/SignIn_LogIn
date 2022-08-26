eb0e39e7354091f18e6221cea1c35178151d5ceb
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2022-08-26 21:28:26 +0530

    Added Config File 🎨

---
+++ Content/__pycache__/config.py
@@ -0,0 +1,10 @@
+
+config = {'host' : 'localhost','dbname' : 'For_Practice','user' : 'postgres','password' : '321654','port' : 5432}
+
+
+
+# host = 'localhost',
+# dbname = 'For_Practice',
+# user = 'postgres',
+# password = '321654',
+# port = 5432
